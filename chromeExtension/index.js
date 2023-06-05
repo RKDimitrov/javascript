5603d929e868f513ff534565c2c770074edb3bbd
RKDimitrov <asrr3fl3x@gmail.com>
2023-06-05 23:53:55 +0300

    localStorage

--- chromeExtension/index.js
+++ chromeExtension/index.js
@@ -4,21 +4,26 @@ const inputBtn = document.querySelector("#input-btn");
 const inputEl = document.querySelector("#input-el");
 const ulEl = document.querySelector("#ul-el");
 
+let leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
+
 inputBtn.addEventListener("click", function () {
     if (!inputEl.value) {
         console.log("Nothing to save");
     } else {
         myLeads.push(inputEl.value);
+        localStorage.setItem("myLeads", JSON.stringify(myLeads));
         inputEl.value = "";
     }
-    renderLeads();
+    if (leadsFromLocalStorage) {
+        myLeads = leadsFromLocalStorage
+        renderLeads();
+    }
 })
 
 function renderLeads() {
     let listItems = "";
 
     for (let i = 0; i < myLeads.length; i++) {
-        //listItems += "<li><a target='_blank' rel='noopener noreferrer' href='" + myLeads[i] + "'>" + myLeads[i] + "</a></li>";
         listItems += `
         <li>
             <a target='_blank' rel='noopener noreferrer' href='${myLeads[i]}'>
